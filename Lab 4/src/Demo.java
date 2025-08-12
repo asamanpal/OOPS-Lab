daedf899a33f5429a1878f6c9e7d8fe985f37227
asamanpal <asamanpal143@gmail.com>
2025-08-12 05:59:02 +0530

    demo

--- Lab 4/src/Demo.java
+++ Lab 4/src/Demo.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+// 1. WAP to initialize Person class instance vaeriable in Demo Class .
+import java.util.Scanner;
+class Person {
+    String name;
+    int age;
+    void person() {
+        System.out.println("Hello you are " + name + " and you are " + age + " years old.");
+    }
+}
+class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Person p = new Person();
+        System.out.print("Enter name: ");
+        p.name = sc.nextLine();
+        System.out.print("Enter age: ");
+        p.age = sc.nextInt();
+        p.person();
 
-public class Demo {
+    }
 }
