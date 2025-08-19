efb205fb81ee7217a8a16b6d75c2d1f83615fc29
asamanpal <asamanpal143@gmail.com>
2025-08-19 05:36:43 +0530

    ashu

---
+++ Lab 5/src/MethodOverPara.java
@@ -0,0 +1,20 @@
+// Java program to demonstrate method overloading with different number of parameters
+
+public class MethodOverPara {
+    public int multiply(int a, int b) {
+        System.out.println("multiply(int, int) called");
+        return a * b;
+    }
+    public int multiply(int a, int b, int c) {
+        System.out.println("multiply(int, int, int) called");
+        return a * b * c;
+    }
+
+    public static void main(String[] args) {
+        MethodOverPara mop = new MethodOverPara();
+        int result1 = mop.multiply(2, 3);
+        int result2 = mop.multiply(2, 3, 4);
+        System.out.println("Result of multiply(2, 3): " + result1);
+        System.out.println("Result of multiply(2, 3, 4): " + result2);
+    }
+}
