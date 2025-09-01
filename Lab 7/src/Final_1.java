ef76f9350e9cf2231b83e512cafa8de5db61d243
asamanpal <asamanpal143@gmail.com>
2025-09-02 05:29:18 +0530

    error

---
+++ Lab 7/src/Final_1.java
@@ -0,0 +1,20 @@
+// Aim: Write a Java Program Using final to Prevent Inheritance.
+
+final class Shape {
+    void printArea() {
+        System.out.println("This is a final class Shape");
+    }
+}
+
+class Circle extends Shape {
+    void printArea() {
+        System.out.println("This is a Circle");
+    }
+}
+
+public class Final_1 {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.display();
+    }
+}
